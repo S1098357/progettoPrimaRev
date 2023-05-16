67d7b986311c7dedf2e6f6aeba8b35337288a7cb
S1098357 <s1098357@studenti.univpm.it>
2023-05-16 13:05:10 +0200

    piccola correzione

--- resources/views/modificaProfilo.blade.php
+++ resources/views/modificaProfilo.blade.php
@@ -5,22 +5,25 @@
 <link rel="stylesheet" type="text/css" href="{{URL('css\profile.css') }}">
 @section('content')
 
+    @if(Auth::check())
+        {{$utente=Auth::user()}}
+
     <center><form>
         <h2>Modifica i tuoi dati personali</h2>
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username"><br><br>
+        <input type="text" id="username" name="username">{{$utente['username']}}<br><br>
         <label for="username">Password:</label>
-        <input type="text" id="password" name="password"><br><br>
+        <input type="text" id="password" name="password">{{$utente['password']}}<br><br>
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email"><br><br>
+        <input type="email" id="email" name="email">{{$utente['email']}}<br><br>
         <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome"><br><br>
+        <input type="text" id="nome" name="nome">{{$utente['nome']}}<br><br>
         <label for="cognome">Cognome:</label>
-        <input type="text" id="cognome" name="cognome"><br><br>
+        <input type="text" id="cognome" name="cognome">{{$utente['cognome']}}<br><br>
         <label for="telefono">Telefono:</label>
-        <input type="tel" id="telefono" name="telefono"><br><br>
-        <label for="data-nascita">Data di nascita:</label>
-        <input type="date" id="datanascita" name="datanascita"><br><br>
+        <input type="tel" id="telefono" name="telefono">{{$utente['telefono']}}<br><br>
+        <label for="datadinascita">Data di nascita:</label>
+        <input type="date" id="datadinascita" name="datadinascita">{{$utente['datadinascita']}}<br><br>
         <label for="genere">Genere:</label>
         <select id="genere" name="genere">
             <option value="maschio">Maschio</option>
@@ -29,4 +32,5 @@
         </select><br><br>
         <button type="submit">Salva Modifiche</button>
     </form></center>
+@endif
 @endsection
