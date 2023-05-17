811bba9d17357d0d36144f55d5e885ab3fd2e930
S1098357 <s1098357@studenti.univpm.it>
2023-05-17 13:08:39 +0200

    piccola correzione

--- resources/views/modificaProfilo.blade.php
+++ resources/views/modificaProfilo.blade.php
@@ -11,19 +11,19 @@
             @csrf
         <h2>Modifica i tuoi dati personali</h2>
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username" placeholder="{{$utente['username']}}"><br><br>
+        <input type="text" id="username" name="username" value="{{$utente['username']}}"><br><br>
         <label for="username">Password:</label>
         <input type="text" id="password" name="password"><br><br>
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="{{$utente['email']}}"><br><br>
+        <input type="email" id="email" name="email" value="{{$utente['email']}}"><br><br>
         <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" placeholder=" {{$utente['nome']}}"><br><br>
+        <input type="text" id="nome" name="nome" value=" {{$utente['nome']}}"><br><br>
         <label for="cognome">Cognome:</label>
-        <input type="text" id="cognome" name="cognome" placeholder="{{$utente['cognome']}}"><br><br>
+        <input type="text" id="cognome" name="cognome" value="{{$utente['cognome']}}"><br><br>
         <label for="telefono">Telefono:</label>
-        <input type="tel" id="telefono" name="telefono" placeholder="{{$utente['telefono']}}"><br><br>
+        <input type="tel" id="telefono" name="telefono" value="{{$utente['telefono']}}"><br><br>
         <label for="datadinascita">Data di nascita:</label>
-        <input type="date" id="datadinascita" name="datadinascita" placeholder="{{$utente['datadinascita']}}"><br><br>
+        <input type="date" id="datadinascita" name="datadinascita" value="{{$utente['datadinascita']}}"><br><br>
         <label for="genere">Genere:</label>
         <select id="genere" name="genere">
             <option value="maschio">Maschio</option>
