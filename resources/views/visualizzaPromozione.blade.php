7bdc77c247bd3da1724a282b48dffb2986812677
S1098357 <s1098357@studenti.univpm.it>
2023-05-19 17:39:37 +0200

    piccola correzione

--- resources/views/visualizzaPromozione.blade.php
+++ resources/views/visualizzaPromozione.blade.php
@@ -7,16 +7,21 @@
 
 @section('content')
 
+    <body>
+
     @if(session()->has('couponRichiesto'))
-        <div><p>Nome Offerta:{{session('couponRichiesto')['idCoupon']}}</p></div>
-        <div><p>Azienda: {{session('couponRichiesto')['idAzienda']}}</p></div>
-        <div><p>Descrizione offerta:{{session('couponRichiesto')['oggetto']}}</p></div>
-        <div><p>Modalità offerta: {{session('couponRichiesto')['modalità']}}</p></div>
-        <div><p>Sconto: {{session('couponRichiesto')['scontistica']}}</p></div>
-        <div><p>QrCode: {{session('couponRichiesto')['qrCode']}}</p></div>
-        <div><p>Usufruibile presso: {{session('couponRichiesto')['luogoFruizione']}}</p></div>
-        <div><p>Nel periodo: {{session('couponRichiesto')['tempoFruizione']}}</p></div>
+        <div class="infoOfferta">
+            <div class="offerta"><p>Nome Offerta: {{session('couponRichiesto')['idCoupon']}}</p></div>
+            <div><p>Azienda: {{session('couponRichiesto')['idAzienda']}}</p></div>
+            <div><p>Descrizione offerta: {{session('couponRichiesto')['oggetto']}}</p></div>
+            <div><p>Modalità offerta: {{session('couponRichiesto')['modalità']}}</p></div>
+            <div><p>Sconto: {{session('couponRichiesto')['scontistica']}}</p></div>
+            <div><p>QrCode: {{session('couponRichiesto')['qrCode']}}</p></div>
+            <div><p>Usufruibile presso: {{session('couponRichiesto')['luogoFruizione']}}</p></div>
+            <div><p>Nel periodo: {{session('couponRichiesto')['tempoFruizione']}}</p></div>
+        </div>
     @endif
+    </body>
 
 @endsection
 </html>
