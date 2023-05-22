ef74a437f8989fdc680bff3387770f67f84c52f6
S1098357 <s1098357@studenti.univpm.it>
2023-05-22 13:09:48 +0200

    piccola modifica

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('cognome');
             $table->string('genere');
-            $table->string('role');
+            $table->string('role')->default('user');
         });
 
 
