1048e443801502c993eef8c7b5492b33a32ccc21
S1098357 <s1098357@studenti.univpm.it>
2023-05-31 10:40:08 +0200

    piccoli hotfix

--- app/Http/Controllers/publicController.php
+++ app/Http/Controllers/publicController.php
@@ -21,7 +21,6 @@ class publicController extends Controller{
 
     public function catalogo(){
         $promozioni=DB::Table('coupons')->get();
-        $couponSalvati=DB::Table('emissione_coupon')->where('idUtente', Auth::user()->id)->get();
         $listaPromozioni=[];
         foreach ($promozioni as $promozione){
             $dataScadenza=new DateTime($promozione->dataScadenza);
@@ -31,14 +30,19 @@ class publicController extends Controller{
                 array_push($listaPromozioni, $promozione);
             }
         }
-        foreach ($couponSalvati as $coupon){
-            for ($i=0;$i<=sizeof($listaPromozioni)-1; $i++) {
-                if ($coupon->idCoupon == $listaPromozioni[$i]->idCoupon) {
-                    array_splice($listaPromozioni,$i,1);
+        if(Auth::check()) {
+            $couponSalvati=DB::Table('emissione_coupon')->where('idUtente', Auth::user()->id)->get();
+            foreach ($couponSalvati as $coupon) {
+                for ($i = 0; $i <= sizeof($listaPromozioni) - 1; $i++) {
+                    if ($coupon->idCoupon == $listaPromozioni[$i]->idCoupon) {
+                        array_splice($listaPromozioni, $i, 1);
+                    }
                 }
             }
+            return view('catalogo', ['listaPromozioni' => $listaPromozioni]);
+        }else{
+            return view('catalogo', ['listaPromozioni' => $listaPromozioni]);
         }
-        return view('catalogo', ['listaPromozioni'=>$listaPromozioni]);
     }
 
     public function info(){
