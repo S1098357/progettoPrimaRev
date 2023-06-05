e4af86e8bd9058c191f73f8f23005b0946541898
S1098357 <s1098357@studenti.univpm.it>
2023-06-05 13:18:59 +0200

    hotfix filtri

---
+++ public/js/listaPromozioni.js
@@ -0,0 +1,11 @@
+$(function aperturaFiltri(){
+    $('#filter-button').click(function(){
+        $('#filtri').slideToggle();
+    })
+})
+
+$(function chiusuraFiltri(){
+    $('#close-button').click(function(){
+        $('#filtri').slideToggle();
+    })
+})
