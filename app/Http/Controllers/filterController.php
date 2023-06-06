566cd21e5deb2ae5bb1a39871233c41c250d6179
S1098357 <s1098357@studenti.univpm.it>
2023-06-06 19:53:00 +0200

    filtri fixati

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Coupon;
+use App\Models\Azienda;
+use App\Models\Promozione;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,125 +12,25 @@ use Illuminate\Support\Facades\DB;
 
 class filterController extends Controller
 {
-
-    public function filtri()
-    {
-        $promozioni=DB::Table('coupons')->get();
-        $listaPromozioni=[];
-        foreach ($promozioni as $promozione){
-            $dataScadenza=new DateTime($promozione->dataScadenza);
-            $dataOdierna= new DateTime(date("Y-m-d"));
-
-            if($dataOdierna<=$dataScadenza){
-                array_push($listaPromozioni, $promozione);
-            }
-        }
-        return view('catalogo', ['listaPromozioni'=>$listaPromozioni]);
-    }
-
-
-    public function filtriPost(Request $request)
-    {
-        $request->validate([
-            'ricercaParola',
-            'ricercaAzienda'
-        ]);
-
-        $ricercaP['ricercaParola'] = $request->ricercaParola;
-        $ricercaA['ricercaAzienda'] = $request->ricercaAzienda;
-
-        // $azienda=$request->only('idAzienda');
-        $info = Coupon::all();
-        $infoAzienda = Coupon::where('idAzienda', $ricercaA)->get();
-
-        $couponPassati = [];
-
-        if ($ricercaP['ricercaParola'] != '' and sizeof($infoAzienda) != 0) {
-            for ($i = 0; $i <= sizeof($infoAzienda) - 1; $i++) {
-                if (str_contains($infoAzienda[$i]['oggetto'], $ricercaP['ricercaParola'])) {
-                    array_push($couponPassati, $infoAzienda[$i]);
-                }
-            }
-        } elseif ($ricercaP['ricercaParola'] != '' and sizeof($infoAzienda) == 0) {
-            for ($i = 0; $i <= sizeof($info) - 1; $i++) {
-                if (str_contains($info[$i]['oggetto'], $ricercaP['ricercaParola'])) {
-                    array_push($couponPassati, $info[$i]);
-                }
-            }
-        } elseif ($ricercaP['ricercaParola'] == '' and sizeof($infoAzienda) != 0) {
-            for ($i = 0; $i <= sizeof($infoAzienda) - 1; $i++) {
-                array_push($couponPassati, $infoAzienda[$i]);
-            }
-        }
-
-        if (sizeof($couponPassati) == 0) {
-            return redirect(route('catalogo'))->with("Errore", "ERRORE, LA RICERCA EFFETTUATA NON HA PRODOTTO RISULTATI VALIDI");
-        } else {
-            return redirect()->intended(route('catalogo'))->with('couponPassati', $couponPassati);
-        }
-    }
-
-        public function filter(Request $request){
-        $output="";
-        if ($request->ricercaAzienda!='')
-        {
-            $filteredCoupons= Coupon::where('idAzienda','Like','%'.$request->ricercaAzienda.'%')->get();
-            foreach ($filteredCoupons as $filteredCoupon){
-                $output.=
-                    '<div class="promozione">
-             <p>Nome Offerta: '.$filteredCoupon->idAzienda.'</p>
-             <p>Oggetto Offerta:'.$filteredCoupon->oggetto.'</p>
-            </div>';
-            }
-            return response($output);
-        }else{
-            $output='<div class="promozione"></div>';
-            return response($output);
-        }
-
-    }
-
-
-
-        public function filter2(Request $request){
-
-        $output="";
-        if ($request->ricercaParola!='') {
-            $filteredCoupons = Coupon::where('oggetto', 'Like', '%' . $request->ricercaParola . '%')->get();
-            foreach ($filteredCoupons as $filteredCoupon) {
-                $output .=
-                    '<div class="promozione">
-             <p>Nome Offerta: ' . $filteredCoupon->idAzienda . '</p>
-             <p>Oggetto Offerta:' . $filteredCoupon->oggetto . '</p>
-            </div>';
-            }
-            return response($output);
-        }else{
-            $output='<div class="promozione"></div>';
-            return response(null);
-        }
-    }
-
-
-        public function filter3(Request $request){
+    public function filter(Request $request){
         $output = "";
         if ($request->ricercaAzienda!=''&& $request->ricercaParola==''){
-            $filteredCoupons= Coupon::where('idAzienda','Like','%'.$request->ricercaAzienda.'%')->get();
+            $filteredCoupons= DB::table('promozione')->join('aziendas', 'promozione.idAzienda', '=', 'aziendas.idAzienda')->where('nomeAzienda','Like','%'.$request->ricercaAzienda.'%')->get();
             foreach ($filteredCoupons as $filteredCoupon){
                 $output.=
                     '<div class="promozione">
-             <p>Nome Offerta: '.$filteredCoupon->idAzienda.'</p>
+             <p>Nome Offerta: '.$filteredCoupon->nomePromozione.'</p>
              <p>Oggetto Offerta:'.$filteredCoupon->oggetto.'</p>
             </div>';
             }
             return response($output);
         }
-        if ($request->ricercaParola!=''&&$request->ricercaAzienda==''){
-            $filteredCoupons = Coupon::where('oggetto', 'Like', '%' . $request->ricercaParola . '%')->get();
+        if ($request->ricercaParola!='' && $request->ricercaAzienda==''){
+            $filteredCoupons = Promozione::where('oggetto', 'Like', '%' . $request->ricercaParola . '%')->get();
             foreach ($filteredCoupons as $filteredCoupon) {
                 $output .=
                     '<div class="promozione">
-             <p>Nome Offerta: ' . $filteredCoupon->idAzienda . '</p>
+             <p>Nome Offerta: ' . $filteredCoupon->nomePromozione . '</p>
              <p>Oggetto Offerta:' . $filteredCoupon->oggetto . '</p>
             </div>';
             }
@@ -137,24 +38,27 @@ class filterController extends Controller
         }
         $listaCoupon=[];
         if ($request->ricercaParola && $request->ricercaAzienda) {
-            $filteredCouponsbyName = Coupon::where('idAzienda', 'Like', '%' . $request->ricercaAzienda . '%')->get();
-            $filteredCouponsbyWords = Coupon::where('oggetto', 'Like', '%' . $request->ricercaParola . '%')->get();
+            $filteredCouponsbyName= DB::table('promozione')->join('aziendas', 'promozione.idAzienda', '=', 'aziendas.idAzienda')->where('nomeAzienda','Like','%'.$request->ricercaAzienda.'%')->get();
+            $filteredCouponsbyWords = Promozione::where('oggetto', 'Like', '%' . $request->ricercaParola . '%')->get();
             foreach ($filteredCouponsbyWords as $filteredCouponbyWords) {
-                foreach ($filteredCouponsbyName as $filteredCouponbyName) {
-                    if ($filteredCouponbyName == $filteredCouponbyWords) {
-                        array_push($listaCoupon, $filteredCouponbyName);
+                foreach ($filteredCouponsbyName as $filteredCouponbyName){
+                    if ($filteredCouponbyName->idPromozione==$filteredCouponbyWords->idPromozione){
+                        array_push($listaCoupon,$filteredCouponbyName);
                     }
                 }
             }
             foreach ($listaCoupon as $coupon){
                 $output .=
                     '<div class="promozione">
-                    <p>Nome Offerta: ' . $coupon->idAzienda . '</p>
-                    <p>Oggetto Offerta:' . $coupon->oggetto . '</p>
-                 </div>';
+                   <p>Nome Offerta: ' . $coupon->nomePromozione . '</p>
+                   <p>Oggetto Offerta:' . $coupon->oggetto . '</p>
+                </div>';
             }
+        }
 
-        }
-            return response($output);
-        }
+        return response($output);
+    }
+
+
+
 }
--- app/Http/Controllers/promozioniController.php
+++ app/Http/Controllers/promozioniController.php
@@ -2,127 +2,169 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Coupon;
+use App\Http\Requests\promozioneRequest;
+use App\Models\Promozione;
+use App\Models\emissione_coupon;
+use DateTime;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class promozioniController extends Controller
 {
-    public function promozioni()
-    {
-        return view('promozioni');
-    }
 
-    public function modificaPromozione()
+    public function listaPromozioni()
     {
-        return view('modificaPromozione');
-    }
 
-    public function modificaPromozionePost(Request $request)
-    {
-        $request->validate(['idCoupon']);
-        $couponRichiesto=null;
+        $promozioni = DB::table('promozione')
+            ->join('aziendas', 'promozione.idAzienda', '=', 'aziendas.idAzienda')
+            ->select('promozione.*','aziendas.nomeAzienda as nomeAzienda')
+            ->get();
+        #$promozioni = DB::Table('promozione')->get();
+        $listaPromozioni = [];
+        foreach ($promozioni as $promozione) {
+            $dataScadenza = new DateTime($promozione->dataScadenza);
+            $dataOdierna = new DateTime(date("Y-m-d"));
 
-        $data['idCoupon'] = $request->idCoupon;
-        $info = Coupon::all();
-        for ($i = 0; $i <= sizeof($info)-1; $i++) {
-            if ($info[$i]['idCoupon']==$data['idCoupon']) {
-                $couponRichiesto=$info[$i];
+            if ($dataOdierna <= $dataScadenza) {
+                array_push($listaPromozioni, $promozione);
             }
         }
+        if (!Auth::check()) {
+            return view('CRUDPromozioni/listaPromozioni', ['listaPromozioni' => $listaPromozioni]);
+        } else if (Gate::allows('isUser')) {
+            $couponSalvati = DB::Table('emissione_coupon')->where('idUtente', Auth::user()->id)->get();
+            foreach ($couponSalvati as $coupon) {
+                for ($i = 0; $i <= sizeof($listaPromozioni) - 1; $i++) {
+                    if ($coupon->idPromozione == $listaPromozioni[$i]->idPromozione) {
+                        array_splice($listaPromozioni, $i, 1);
+                    }
 
-        return redirect()->intended(route('modificaPromozione'))->with('promozione', $couponRichiesto);
-    }
-
-    public function visualizzaPromozionePost(Request $request)
-    {
-        $request->validate(['idCoupon']);
-
-        $data['idCoupon'] = $request->idCoupon;
-
-        $info = Coupon::all();
-        $couponRichiesto = $info[0];
-
-        for ($i = 0; $i <= sizeof($info)-1; $i++) {
-            if ($info[$i]['idCoupon']==$data['idCoupon']) {
-                $couponRichiesto=$info[$i];
+                }
             }
-        }
-
-        return redirect()->intended(route('visualizzaPromozione'))->with('couponRichiesto', $couponRichiesto);
+            return view('CRUDPromozioni/listaPromozioni', ['listaPromozioni'=>$listaPromozioni]);
+        } else
+            return view('CRUDPromozioni/listaPromozioni', ['listaPromozioni' => $listaPromozioni]);
     }
 
-    public function creaPromozione()
-    {
-        return view('creaPromozione');
+
+
+
+
+    public function modificaPromozione(Request $request){
+        $promozione=DB::Table('promozione')
+            ->where('idPromozione', $request->idPromozione)->get();
+        $option= 'edit';
+        return view('CRUDPromozioni/promozioneDesigner', ['promozione'=>$promozione], ['option'=>$option]);
     }
 
-    public function creaPromozionePost(Request $request)
+
+    public function promozioneCreator(){
+        return view('CRUDPromozioni/promozioneDesigner', ['option'=>'create']);
+    }
+
+    public function visualPromozione(Request $request){
+
+        $info = DB::table('promozione')
+            ->join('aziendas', 'promozione.idAzienda', '=', 'aziendas.idAzienda')
+            ->where('promozione.idPromozione', $request->idPromozione)
+            ->select('promozione.*','aziendas.nomeAzienda as nomeAzienda')
+            ->get();
+        return view('CRUDPromozioni/promozione',['info'=> $info]);
+    }
+
+    public function eliminaPromozione(Request $request){
+        DB::delete('delete from promozione where idPromozione = ?',[$request->idPromozione]);
+        return redirect(route('listaPromozioni'));
+    }
+
+    public function editPromozione(promozioneRequest $request)
     {
 
-        $request->validate([
-            'idCoupon',
-            'oggetto',
-            'modalità',
-            'scontistica',
-            'luogoFruizione',
-            'Azienda',
-        ]);
-
-        $data['idCoupon'] = $request->idCoupon;
+        $data['nomePromozione'] = $request->nomePromozione;
         $data['oggetto'] = $request->oggetto;
         $data['modalità'] = $request->modalità;
         $data['scontistica'] = $request->scontistica;
         $data['luogoFruizione'] = $request->luogoFruizione;
-        $data['idAzienda'] = $request->Azienda;
-        $data['qrCode'] = 'prova';
-        $data['tempoFruizione'] = '2h';
-        $Coupon = Coupon::create($data);
+        $data['idAzienda'] = $request->idAzienda;
+        $data['dataScadenza'] = $request->dataScadenza;;
 
-        return redirect(route('Promozioni'));
+        Promozione::where('idPromozione',$request->id)->update($data);
+        return redirect(route('listaPromozioni'));
     }
 
-
-        public function modificaPromozioneFinale(Request $request)
+    public function creaPromozione(promozioneRequest $request)
     {
 
-        $request->validate([
-            'idCoupon',
-            'oggetto',
-            'modalità',
-            'scontistica',
-            'luogoFruizione',
-            'Azienda',
-        ]);
-
-        $data['idCoupon'] = $request->idCoupon;
+        $data['nomePromozione'] = $request->nomePromozione;
         $data['oggetto'] = $request->oggetto;
         $data['modalità'] = $request->modalità;
         $data['scontistica'] = $request->scontistica;
         $data['luogoFruizione'] = $request->luogoFruizione;
-        $data['idAzienda'] = $request->Azienda;
-        $data['qrCode']='prova';
-        $data['tempoFruizione']='2h';
+        $data['idAzienda'] = $request->idAzienda;
+        $data['dataScadenza'] = $request->dataScadenza;;
+        Promozione::create($data);
 
-        Coupon::where('idCoupon',$data['idCoupon'])->delete();
-        $Coupon= Coupon::create($data);
+        return redirect(route('listaPromozioni'));
 
-        return redirect(route('Promozioni'));
     }
 
-    public function eliminaPromozione(Request $request)
+    public function salvaCoupon(Request $request){
+        $data['idPromozione']=$request->idPromozione;
+        $data['idUtente']=Auth::user()->id;
+        $data['dataEmissione']=date('Y-m-d');
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 10; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        $data['codice']=$randomString;
+        emissione_coupon::create($data);
+        $listaId=DB::Table('emissione_coupon')
+            ->where('idUtente', Auth::user()->id)->get();
+        $Coupons=DB::Table('promozione')->where('idPromozione', $request->idPromozione)->get();
+        $codice=null;
+        foreach ($listaId as $coupon){
+            if ($coupon->idPromozione==$request->idPromozione){
+                $codice=$coupon->codice;
+            }
+        }
+        return view('couponSalvati',['promozione'=>$Coupons],['codice'=>$codice]);
+    }
+
+    public function couponSalvati()
     {
-
-        $request->validate([
-            'idCoupon'
-        ]);
-
-        $data['idCoupon'] = $request->idCoupon;
-
-        Coupon::where('idCoupon',$data['idCoupon'])->delete();
-
-        return redirect(route('Promozioni'));
+        $listaId=DB::Table('emissione_coupon')
+            ->where('idUtente', Auth::user()->id)->get();
+        $tuttiCoupon=DB::Table('promozione')->get();
+        $listaCoupon=[];
+        $dataOdierna= new DateTime(date("Y-m-d"));
+        $listaCodici=[];
+        foreach ($listaId as $id){
+            foreach ($tuttiCoupon as $coupon){
+                $dataScadenza=new DateTime($coupon->dataScadenza);
+                if($coupon->idPromozione==$id->idPromozione and $dataOdierna<=$dataScadenza){
+                    array_push($listaCodici,$id->codice);
+                    array_push($listaCoupon,$coupon);
+                }
+            }
+        }
+        return view('couponSalvati',['listaCoupon'=>$listaCoupon],['listaCodici'=>$listaCodici]);
     }
 
 
-
+    public function couponSingolo(Request $request){
+        $listaId=DB::Table('emissione_coupon')
+            ->where('idUtente', Auth::user()->id)->get();
+        $Coupons=DB::Table('promozione')->where('idPromozione', $request->idPromozione)->get();
+        $codice=null;
+        foreach ($listaId as $coupon){
+            if ($coupon->idPromozione==$request->idPromozione){
+                $codice=$coupon->codice;
+            }
+        }
+        return view('couponSalvati',['promozione'=>$Coupons],['codice'=>$codice]);
+    }
 }
